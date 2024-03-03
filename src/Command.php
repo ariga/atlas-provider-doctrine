89015ccfbab6be9b30dbb2892e06f86aa4829ece
Ronen Lubin <ronen7822@gmail.com>
2024-03-03 10:44:41 +0200

    src: create cli command to dump sql

---
+++ src/Command.php
@@ -0,0 +1,48 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+require "LoadEntities.php";
+
+// AtlasCommand dump the sql of doctrine entities similar to the doctrine command: orm:schema-tool:create --dump-sql
+// but without requiring a real database connection
+class AtlasCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('atlas:dump-sql')
+            ->addOption(
+                'dialect',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Select the DB dialect to use: "mysql", "postgres", "sqlite"',
+                "mysql"
+            )
+            ->addOption(
+                'path',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Path to the entities directory',
+                __DIR__."/src"
+            )
+            ->setDescription('Dumps the SQL describing the entities schema to the console');
+    }
+
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $ui = new SymfonyStyle($input, $output);
+        try {
+            $sql = DumpDDL([$input->getOption('path')], $input->getOption('dialect'));
+        } catch (Exception $e) {
+            $ui->error($e->getMessage());
+            return 1;
+        }
+        $ui->writeln($sql);
+        return 0;
+    }
+};
---
+++ tests/CommandTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require "src/Command.php";
+
+final class CommandTest extends TestCase
+{
+
+    public function testCommand(): void
+    {
+        $output = shell_exec("php bin/doctrine atlas:dump-sql --dialect mysql --path ./tests/entities");
+        $expected = 'CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE bugs (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(255) NOT NULL, created DATETIME NOT NULL, status VARCHAR(255) NOT NULL, engineer_id INT DEFAULT NULL, reporter_id INT DEFAULT NULL, INDEX IDX_1E197C9F8D8CDF1 (engineer_id), INDEX IDX_1E197C9E1CFE6F5 (reporter_id), PRIMARY KEY(id));
+ALTER TABLE bugs ADD CONSTRAINT FK_1E197C9F8D8CDF1 FOREIGN KEY (engineer_id) REFERENCES users (id);
+ALTER TABLE bugs ADD CONSTRAINT FK_1E197C9E1CFE6F5 FOREIGN KEY (reporter_id) REFERENCES users (id);
+';
+        $this->assertEquals($expected, $output);
+    }
+
+    public function testCommandInvalidDialect(): void
+    {
+        exec("php bin/doctrine atlas:dump-sql --dialect bad_dialect --path ./tests/entities", $output, $return_var);
+        $this->assertEquals(1, $return_var);
+        // check that stderr contains the expected error message
+        $this->assertStringContainsString("Invalid dialect: bad_dialect", $output[1]);
+    }
+
+    public function testCommandInvalidPath(): void
+    {
+        exec("php bin/doctrine atlas:dump-sql --dialect mysql --path /bad/path", $output, $return_var);
+        $this->assertEquals(1, $return_var);
+        $this->assertStringContainsString("Invalid path: /bad/path", $output[1]);
+    }
+}
