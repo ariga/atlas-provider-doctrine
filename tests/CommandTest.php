431fc9f0704aa7dcf428c48111f44510c64cca9a
luantranminh <tranminhluan1206@gmail.com>
2025-06-09 17:14:07 +0700

    tests: update tests to use data providers

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -6,16 +6,24 @@ require_once "src/Command.php";
 
 final class CommandTest extends TestCase
 {
-
-    public function testCommand(): void
+    /**
+     * @dataProvider dialectProvider
+     */
+    public function testCommand(string $dialect, string $expectedFile): void
     {
-        $output = shell_exec("php tests/bin/doctrine atlas:schema --dialect mysql --path ./tests/entities/regular");
-        $expected = 'CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id));
-CREATE TABLE bugs (id INT AUTO_INCREMENT NOT NULL, description VARCHAR(255) NOT NULL, created DATETIME NOT NULL, status VARCHAR(255) NOT NULL, engineer_id INT DEFAULT NULL, reporter_id INT DEFAULT NULL, INDEX IDX_1E197C9F8D8CDF1 (engineer_id), INDEX IDX_1E197C9E1CFE6F5 (reporter_id), PRIMARY KEY(id));
-ALTER TABLE bugs ADD CONSTRAINT FK_1E197C9F8D8CDF1 FOREIGN KEY (engineer_id) REFERENCES users (id);
-ALTER TABLE bugs ADD CONSTRAINT FK_1E197C9E1CFE6F5 FOREIGN KEY (reporter_id) REFERENCES users (id);
-';
+        $output = shell_exec("php tests/bin/doctrine atlas:schema --dialect $dialect --path ./tests/entities/regular");
+        $expected = file_get_contents(__DIR__ . "/data/$expectedFile");
         $this->assertEquals($expected, $output);
+    }
+
+    public static function dialectProvider(): array
+    {
+        return [
+            'mysql' => ['mysql', 'regular_mysql.sql'],
+            'postgres' => ['postgres', 'regular_postgres.sql'],
+            'sqlite' => ['sqlite', 'regular_sqlite.sql'],
+            'sqlserver' => ['sqlserver', 'regular_sqlserver.sql'],
+        ];
     }
 
     public function testCommandInvalidDialect(): void
